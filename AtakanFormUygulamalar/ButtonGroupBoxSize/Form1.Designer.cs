433b1bdd6b09f832676dfee7c15baa373a32459a
Atakan <atakanbektas.c@gmail.com>
2023-02-03 10:51:42 +0300

    ScrollBar Horizontal devre dışı bırakıldı

---
+++ AtakanFormUygulamalar/ButtonGroupBoxSize/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace ButtonGroupBoxSize
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.cboxAdet = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.groupBox1);
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.button4);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(13, 14);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(410, 318);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(25, 25);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(350, 158);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 218);
+            this.button1.Margin = new System.Windows.Forms.Padding(25, 10, 0, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 37);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Topla";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(170, 218);
+            this.button4.Margin = new System.Windows.Forms.Padding(25, 10, 0, 10);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(120, 37);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Çıkar";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(25, 275);
+            this.button2.Margin = new System.Windows.Forms.Padding(25, 10, 0, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Böl";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(170, 275);
+            this.button3.Margin = new System.Windows.Forms.Padding(25, 10, 0, 10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 37);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Çarp";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // cboxAdet
+            // 
+            this.cboxAdet.FormattingEnabled = true;
+            this.cboxAdet.Location = new System.Drawing.Point(469, 59);
+            this.cboxAdet.Name = "cboxAdet";
+            this.cboxAdet.Size = new System.Drawing.Size(188, 28);
+            this.cboxAdet.TabIndex = 1;
+            this.cboxAdet.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(469, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 42);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Satırda kaç buton olsun?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 411);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboxAdet);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.ComboBox cboxAdet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ AtakanFormUygulamalar/ButtonGroupBoxSize/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ButtonGroupBoxSize
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+            int adet = int.Parse(cboxAdet.Text) - 1;    
+
+            button1.Width = (groupBox1.Width - button1.Margin.Left * adet)/(adet+1);
+            button2.Width = (groupBox1.Width - button1.Margin.Left * adet)/(adet+1);
+            button3.Width = (groupBox1.Width - button1.Margin.Left * adet)/(adet+1);
+            button4.Width = (groupBox1.Width - button1.Margin.Left * adet)/(adet+1);
+
+
+            flowLayoutPanel1.HorizontalScroll.Maximum = 0;
+            flowLayoutPanel1.AutoScroll = false;
+            flowLayoutPanel1.HorizontalScroll.Visible = false;
+            flowLayoutPanel1.AutoScroll = true;
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+            cboxAdet.Items.Add(1);
+            cboxAdet.Items.Add(2);
+            cboxAdet.Items.Add(3);
+            cboxAdet.Items.Add(4);
+            cboxAdet.SelectedItem = 3;
+            int adet = int.Parse(cboxAdet.Text) - 1;
+            button1.Width = (groupBox1.Width - button1.Margin.Left * adet) / (adet + 1);
+            button2.Width = (groupBox1.Width - button1.Margin.Left * adet) / (adet + 1);
+            button3.Width = (groupBox1.Width - button1.Margin.Left * adet) / (adet + 1);
+            button4.Width = (groupBox1.Width - button1.Margin.Left * adet) / (adet + 1);
+
+ 
+
+
+        }
+    }
+}
