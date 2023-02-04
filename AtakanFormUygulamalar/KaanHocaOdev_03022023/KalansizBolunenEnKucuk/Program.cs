6cd432a3c28ff1f447d17b05dc906be02a05bf59
Atakan <atakanbektas.c@gmail.com>
2023-02-04 11:07:56 +0300

    Tüm sorular tamamlandı.
    Ödev diğer bilgisayarımda kaldığı ve şuan tamirde olduğu için tekrar çözümledim.

--- AtakanFormUygulamalar/KaanHocaOdev_03022023/KalansizBolunenEnKucuk/Program.cs
+++ AtakanFormUygulamalar/KaanHocaOdev_03022023/KalansizBolunenEnKucuk/Program.cs
@@ -14,28 +14,30 @@ namespace KalansizBolunenEnKucuk
             int start = int.Parse(Console.ReadLine());
             Console.WriteLine("Lütfen bitiş sayısı giriniz.");
             int stop = int.Parse(Console.ReadLine());
-
-            getSmallestNumber(start,stop);
+            Console.WriteLine(getSmallestNumber(start, stop));
+            Console.ReadKey();
         }
 
         private static int getSmallestNumber(int start, int stop)
         {
-            int reelMulti = 1;
-            int min = 0;
-            for (int i = start; i <= stop; i++)
+            int sayac = 0;
+            int j = 1;
+            for (int i = stop; i < int.MaxValue; i += stop)
             {
-                reelMulti *= i;
-            }
-
-            for (int j = start; j <=stop; j++)
-            {
-                for (int k=stop; k <=reelMulti; k++)
+                for (j = start; j < stop; j++)
                 {
-                    if (k%j==0)
+                    if (i % j == 0)
                     {
-                        min = k / j;
+                        sayac++;
                     }
-
+                }
+                if (sayac == stop - start) // 2. for'daki if'e her seferinde girdiyse: return i;
+                {
+                    return i;
+                }
+                else
+                {
+                    sayac = 0;
                 }
             }
             return 0;
