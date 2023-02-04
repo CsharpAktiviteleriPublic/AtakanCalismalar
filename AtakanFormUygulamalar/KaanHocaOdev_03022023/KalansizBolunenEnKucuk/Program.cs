30f3e907754010e5dbe49c5bedc38f8c1ce00964
Atakan <atakanbektas.c@gmail.com>
2023-02-04 10:47:05 +0300

    1. Soru hariç diğer 4 soru tamamlandı.

---
+++ AtakanFormUygulamalar/KaanHocaOdev_03022023/KalansizBolunenEnKucuk/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalansizBolunenEnKucuk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen başlangıç sayısı giriniz.");
+            int start = int.Parse(Console.ReadLine());
+            Console.WriteLine("Lütfen bitiş sayısı giriniz.");
+            int stop = int.Parse(Console.ReadLine());
+
+            getSmallestNumber(start,stop);
+        }
+
+        private static int getSmallestNumber(int start, int stop)
+        {
+            int reelMulti = 1;
+            int min = 0;
+            for (int i = start; i <= stop; i++)
+            {
+                reelMulti *= i;
+            }
+
+            for (int j = start; j <=stop; j++)
+            {
+                for (int k=stop; k <=reelMulti; k++)
+                {
+                    if (k%j==0)
+                    {
+                        min = k / j;
+                    }
+
+                }
+            }
+            return 0;
+        }
+
+    }
+}
