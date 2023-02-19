f332c90db36d9e46fe328b0dd72f9be456de4ece
Atakan <atakanbektas.c@gmail.com>
2023-02-19 22:24:37 +0300

    Futbol Simulasyon Oyunu Form Güncelleme.

---
+++ AtakanFormUygulamalar/LabirentOyunu/LabirentOyunu/Form1.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace LabirentOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmrYol = new System.Windows.Forms.Timer(this.components);
+            this.pbT1 = new System.Windows.Forms.PictureBox();
+            this.pbT4 = new System.Windows.Forms.PictureBox();
+            this.pbT5 = new System.Windows.Forms.PictureBox();
+            this.pbT3 = new System.Windows.Forms.PictureBox();
+            this.pbT2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pbT7 = new System.Windows.Forms.PictureBox();
+            this.pbT6 = new System.Windows.Forms.PictureBox();
+            this.pbT8 = new System.Windows.Forms.PictureBox();
+            this.pbT9 = new System.Windows.Forms.PictureBox();
+            this.pbT10 = new System.Windows.Forms.PictureBox();
+            this.pnlAlan = new System.Windows.Forms.Panel();
+            this.pbTop = new System.Windows.Forms.PictureBox();
+            this.pbKarakter = new System.Windows.Forms.PictureBox();
+            this.tmrTop = new System.Windows.Forms.Timer(this.components);
+            this.lblGoal = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblUserScore = new System.Windows.Forms.Label();
+            this.lblComputerScore = new System.Windows.Forms.Label();
+            this.lblGoalKeeper = new System.Windows.Forms.Label();
+            this.tmrGoalKeeper = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbT1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbT4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbT5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbT3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbT2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbT7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbT6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbT8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbT9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbT10)).BeginInit();
+            this.pnlAlan.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbKarakter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tmrYol
+            // 
+            this.tmrYol.Interval = 30;
+            this.tmrYol.Tick += new System.EventHandler(this.tmrYol_Tick);
+            // 
+            // pbT1
+            // 
+            this.pbT1.Image = global::LabirentOyunu.Properties.Resources.tree;
+            this.pbT1.Location = new System.Drawing.Point(15, 3);
+            this.pbT1.Name = "pbT1";
+            this.pbT1.Size = new System.Drawing.Size(151, 135);
+            this.pbT1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbT1.TabIndex = 16;
+            this.pbT1.TabStop = false;
+            // 
+            // pbT4
+            // 
+            this.pbT4.Image = global::LabirentOyunu.Properties.Resources.tree;
+            this.pbT4.Location = new System.Drawing.Point(1269, 3);
+            this.pbT4.Name = "pbT4";
+            this.pbT4.Size = new System.Drawing.Size(151, 135);
+            this.pbT4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbT4.TabIndex = 15;
+            this.pbT4.TabStop = false;
+            // 
+            // pbT5
+            // 
+            this.pbT5.Image = global::LabirentOyunu.Properties.Resources.tree;
+            this.pbT5.Location = new System.Drawing.Point(1687, 3);
+            this.pbT5.Name = "pbT5";
+            this.pbT5.Size = new System.Drawing.Size(151, 135);
+            this.pbT5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbT5.TabIndex = 15;
+            this.pbT5.TabStop = false;
+            // 
+            // pbT3
+            // 
+            this.pbT3.Image = global::LabirentOyunu.Properties.Resources.tree;
+            this.pbT3.Location = new System.Drawing.Point(851, 3);
+            this.pbT3.Name = "pbT3";
+            this.pbT3.Size = new System.Drawing.Size(151, 135);
+            this.pbT3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbT3.TabIndex = 13;
+            this.pbT3.TabStop = false;
+            // 
+            // pbT2
+            // 
+            this.pbT2.Image = global::LabirentOyunu.Properties.Resources.tree;
+            this.pbT2.Location = new System.Drawing.Point(433, 3);
+            this.pbT2.Name = "pbT2";
+            this.pbT2.Size = new System.Drawing.Size(151, 135);
+            this.pbT2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbT2.TabIndex = 14;
+            this.pbT2.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::LabirentOyunu.Properties.Resources.tree;
+            this.pictureBox5.Location = new System.Drawing.Point(2682, 926);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(151, 135);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 12;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::LabirentOyunu.Properties.Resources.tree;
+            this.pictureBox4.Location = new System.Drawing.Point(3576, 926);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(151, 135);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 11;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pbT7
+            // 
+            this.pbT7.Image = global::LabirentOyunu.Properties.Resources.tree;
+            this.pbT7.Location = new System.Drawing.Point(1269, 926);
+            this.pbT7.Name = "pbT7";
+            this.pbT7.Size = new System.Drawing.Size(151, 135);
+            this.pbT7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbT7.TabIndex = 10;
+            this.pbT7.TabStop = false;
+            // 
+            // pbT6
+            // 
+            this.pbT6.Image = global::LabirentOyunu.Properties.Resources.tree;
+            this.pbT6.Location = new System.Drawing.Point(1687, 926);
+            this.pbT6.Name = "pbT6";
+            this.pbT6.Size = new System.Drawing.Size(151, 135);
+            this.pbT6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbT6.TabIndex = 10;
+            this.pbT6.TabStop = false;
+            // 
+            // pbT8
+            // 
+            this.pbT8.Image = global::LabirentOyunu.Properties.Resources.tree;
+            this.pbT8.Location = new System.Drawing.Point(851, 926);
+            this.pbT8.Name = "pbT8";
+            this.pbT8.Size = new System.Drawing.Size(151, 135);
+            this.pbT8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbT8.TabIndex = 9;
+            this.pbT8.TabStop = false;
+            // 
+            // pbT9
+            // 
+            this.pbT9.Image = global::LabirentOyunu.Properties.Resources.tree;
+            this.pbT9.Location = new System.Drawing.Point(433, 926);
+            this.pbT9.Name = "pbT9";
+            this.pbT9.Size = new System.Drawing.Size(151, 135);
+            this.pbT9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbT9.TabIndex = 9;
+            this.pbT9.TabStop = false;
+            // 
+            // pbT10
+            // 
+            this.pbT10.Image = global::LabirentOyunu.Properties.Resources.tree;
+            this.pbT10.Location = new System.Drawing.Point(15, 926);
+            this.pbT10.Name = "pbT10";
+            this.pbT10.Size = new System.Drawing.Size(151, 135);
+            this.pbT10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbT10.TabIndex = 8;
+            this.pbT10.TabStop = false;
+            // 
+            // pnlAlan
+            // 
+            this.pnlAlan.BackColor = System.Drawing.Color.White;
+            this.pnlAlan.Controls.Add(this.lblGoalKeeper);
+            this.pnlAlan.Controls.Add(this.lblComputerScore);
+            this.pnlAlan.Controls.Add(this.lblGoal);
+            this.pnlAlan.Controls.Add(this.pbKarakter);
+            this.pnlAlan.Controls.Add(this.pbTop);
+            this.pnlAlan.Location = new System.Drawing.Point(86, 144);
+            this.pnlAlan.Name = "pnlAlan";
+            this.pnlAlan.Size = new System.Drawing.Size(1825, 776);
+            this.pnlAlan.TabIndex = 1;
+            // 
+            // pbTop
+            // 
+            this.pbTop.Image = global::LabirentOyunu.Properties.Resources.ball;
+            this.pbTop.Location = new System.Drawing.Point(96, 389);
+            this.pbTop.Name = "pbTop";
+            this.pbTop.Size = new System.Drawing.Size(42, 37);
+            this.pbTop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbTop.TabIndex = 1;
+            this.pbTop.TabStop = false;
+            this.pbTop.Visible = false;
+            // 
+            // pbKarakter
+            // 
+            this.pbKarakter.Image = global::LabirentOyunu.Properties.Resources.sag;
+            this.pbKarakter.Location = new System.Drawing.Point(0, 294);
+            this.pbKarakter.Name = "pbKarakter";
+            this.pbKarakter.Size = new System.Drawing.Size(100, 132);
+            this.pbKarakter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbKarakter.TabIndex = 0;
+            this.pbKarakter.TabStop = false;
+            // 
+            // tmrTop
+            // 
+            this.tmrTop.Interval = 50;
+            this.tmrTop.Tick += new System.EventHandler(this.tmrTop_Tick);
+            // 
+            // lblGoal
+            // 
+            this.lblGoal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lblGoal.Location = new System.Drawing.Point(1726, 0);
+            this.lblGoal.Name = "lblGoal";
+            this.lblGoal.Size = new System.Drawing.Size(100, 776);
+            this.lblGoal.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(-10, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 776);
+            this.label1.TabIndex = 3;
+            // 
+            // lblUserScore
+            // 
+            this.lblUserScore.BackColor = System.Drawing.Color.Red;
+            this.lblUserScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblUserScore.Location = new System.Drawing.Point(1, 153);
+            this.lblUserScore.Name = "lblUserScore";
+            this.lblUserScore.Size = new System.Drawing.Size(89, 55);
+            this.lblUserScore.TabIndex = 17;
+            this.lblUserScore.Text = "0";
+            this.lblUserScore.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblComputerScore
+            // 
+            this.lblComputerScore.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lblComputerScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblComputerScore.Location = new System.Drawing.Point(1729, 9);
+            this.lblComputerScore.Name = "lblComputerScore";
+            this.lblComputerScore.Size = new System.Drawing.Size(93, 55);
+            this.lblComputerScore.TabIndex = 18;
+            this.lblComputerScore.Text = "0";
+            this.lblComputerScore.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblGoalKeeper
+            // 
+            this.lblGoalKeeper.BackColor = System.Drawing.Color.Brown;
+            this.lblGoalKeeper.Location = new System.Drawing.Point(1636, 102);
+            this.lblGoalKeeper.Name = "lblGoalKeeper";
+            this.lblGoalKeeper.Size = new System.Drawing.Size(28, 88);
+            this.lblGoalKeeper.TabIndex = 19;
+            // 
+            // tmrGoalKeeper
+            // 
+            this.tmrGoalKeeper.Interval = 10;
+            this.tmrGoalKeeper.Tick += new System.EventHandler(this.tmrGoalKeeper_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(1924, 1061);
+            this.Controls.Add(this.lblUserScore);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbT1);
+            this.Controls.Add(this.pbT4);
+            this.Controls.Add(this.pbT5);
+            this.Controls.Add(this.pbT3);
+            this.Controls.Add(this.pbT2);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pbT7);
+            this.Controls.Add(this.pbT6);
+            this.Controls.Add(this.pbT8);
+            this.Controls.Add(this.pbT9);
+            this.Controls.Add(this.pbT10);
+            this.Controls.Add(this.pnlAlan);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pbT1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbT4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbT5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbT3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbT2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbT7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbT6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbT8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbT9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbT10)).EndInit();
+            this.pnlAlan.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbTop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbKarakter)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pbKarakter;
+        private System.Windows.Forms.Panel pnlAlan;
+        private System.Windows.Forms.Timer tmrYol;
+        private System.Windows.Forms.PictureBox pbT10;
+        private System.Windows.Forms.PictureBox pbT9;
+        private System.Windows.Forms.PictureBox pbT6;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pbT8;
+        private System.Windows.Forms.PictureBox pbT7;
+        private System.Windows.Forms.PictureBox pbT5;
+        private System.Windows.Forms.PictureBox pbT3;
+        private System.Windows.Forms.PictureBox pbT2;
+        private System.Windows.Forms.PictureBox pbT1;
+        private System.Windows.Forms.PictureBox pbT4;
+        private System.Windows.Forms.PictureBox pbTop;
+        private System.Windows.Forms.Timer tmrTop;
+        private System.Windows.Forms.Label lblGoal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblComputerScore;
+        private System.Windows.Forms.Label lblUserScore;
+        private System.Windows.Forms.Label lblGoalKeeper;
+        private System.Windows.Forms.Timer tmrGoalKeeper;
+    }
+}
+
---
+++ AtakanFormUygulamalar/LabirentOyunu/LabirentOyunu/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabirentOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<PictureBox> pbList = new List<PictureBox>();
+        int topHizi;
+        int userScore = 0;
+        int computerScore = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tmrYol.Start();
+            tmrGoalKeeper.Start();
+            pbList.Add(pbT1);
+            pbList.Add(pbT2);
+            pbList.Add(pbT3);
+            pbList.Add(pbT4);
+            pbList.Add(pbT5);
+            pbList.Add(pbT6);
+            pbList.Add(pbT7);
+            pbList.Add(pbT8);
+            pbList.Add(pbT9);
+            pbList.Add(pbT10);
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+
+                case Keys.Up:
+                    if (pbKarakter.Bounds.IntersectsWith(pnlAlan.Bounds))
+                    {
+                        pbKarakter.Top -= 5;
+                    }
+                    break;
+                case Keys.Down:
+                    if (!(pbKarakter.Top + pbKarakter.Height >= pnlAlan.Height))
+                    {
+                        pbKarakter.Top += 5;
+                    }
+                    break;
+                case Keys.Space:
+                    topHizi = 100;
+                    pbTop.Left = pbKarakter.Left+ pbKarakter.Width;
+                    pbTop.Top = pbKarakter.Top + pbKarakter.Height - pbTop.Height;
+                    pbTop.Visible = true;
+                    tmrTop.Start();
+                    break;
+            }
+        }
+        private void tmrYol_Tick(object sender, EventArgs e)
+        {
+
+            foreach (PictureBox pb in pbList)
+            {
+                if (pb.Left + pb.Width <= 0)
+                {
+                    pb.Left = Width;
+                }
+                else
+                    pb.Left -= 3;
+            }
+        }      
+        private void tmrTop_Tick(object sender, EventArgs e)
+        {
+
+
+            if (topHizi > 0 )
+            {
+                if (!(pbTop.Bounds.IntersectsWith(lblGoal.Bounds)))
+                {
+                    topHizi -= 1;
+                    pbTop.Left += topHizi;
+                }
+                else
+                {                   
+                    userScore++;
+                    lblUserScore.Text = (userScore).ToString();
+                    pbTop.Visible = false;
+                    tmrTop.Stop();
+                }
+                
+                
+            }
+            else
+            {
+               
+                pbTop.Visible = false;
+                tmrTop.Stop();
+            }
+        }
+
+        int goalKeeperV = 5;
+        private void tmrGoalKeeper_Tick(object sender, EventArgs e)
+        {
+            lblGoalKeeper.Top += goalKeeperV;
+            if (lblGoalKeeper.Top+lblGoalKeeper.Height>=pnlAlan.Height || lblGoalKeeper.Top<=0)
+            {
+                goalKeeperV = goalKeeperV * -1;
+            }
+            
+        }
+    }
+}
